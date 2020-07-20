55f1d1c2980f8b0b6111719a0e0be5cb7485a034
Dmitry Nor <dnor@lulusoftware.com>
2020-07-20 19:56:22 +0300

    Add login method

--- DatingApp.API/Models/Uesr.cs
+++ DatingApp.API/Models/Uesr.cs
@@ -4,9 +4,7 @@ namespace DatingApp.API.Models
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
         public byte[] PasswordHash { get; set; }
-
         public byte[] PasswordSalt { get; set; }
     }
 }
